4c2384f09edf0dd4767d54bb56e33adab901f533
Ryan Mahler <ryan.mahler@d2l.com>
2016-04-11 12:38:36 -0400

    Add ValenceRoute and tests
    Currently the constructor for VersionedValenceRoute adds leading and trailing slashes to both prefix and suffix - this may be relaxed and/or handled by the URL resolver rather than here (the slashes are required, most notably the trailing slash, but this might be a weird place to deal with that).

---
+++ src/valence-route.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const assert = require('assert');
+
+const ValenceVersion = require('./valence-version');
+
+class ValenceRoute {
+}
+
+class SimpleValenceRoute extends ValenceRoute {
+	// e.g. /d2l/api/versions
+
+	constructor(path) {
+		assert('string' === typeof path, 'path must be a string');
+
+		super();
+
+		this._path = path;
+	}
+
+	get path() {
+		return this._path;
+	}
+}
+
+class VersionedValenceRoute extends ValenceRoute {
+	constructor(prefix, version, suffix) {
+		assert('string' === typeof prefix, 'prefix must be a string');
+		assert(version instanceof ValenceVersion, 'version must be a ValenceVersion');
+		assert('string' === typeof suffix, 'suffix must be a string');
+
+		super();
+
+		this._prefix = prefix.startsWith('/') ? prefix : '/' + prefix;
+		this._prefix = prefix.endsWith('/') ? this._prefix : this._prefix + '/';
+		this._version = version;
+		this._suffix = suffix.startsWith('/') ? suffix : '/' + suffix;
+		this._suffix = suffix.endsWith('/') ? this._suffix : this._suffix + '/';
+	}
+
+	get prefix() {
+		return this._prefix;
+	}
+
+	get version() {
+		return this._version;
+	}
+
+	get suffix() {
+		return this._suffix;
+	}
+}
+
+class LERoute extends VersionedValenceRoute {
+	constructor(versionString, path) {
+		super('/d2l/api/le/', new ValenceVersion('le', versionString), path);
+	}
+}
+
+class LPRoute extends VersionedValenceRoute {
+	constructor(versionString, path) {
+		super('/d2l/api/lp/', new ValenceVersion('lp', versionString), path);
+	}
+}
+
+module.exports = {
+	ValenceRoute: ValenceRoute,
+	SimpleValenceRoute: SimpleValenceRoute,
+	VersionedValenceRoute: VersionedValenceRoute,
+	LERoute: LERoute,
+	LPRoute: LPRoute
+};
